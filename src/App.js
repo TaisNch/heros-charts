1ea7b2b6b32d5ffc59adb9ceddfbc455b1d5a3e3
TaisNch <tais.aliexpress@gmail.com>
2024-06-05 15:25:00 +0200

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import './Card.css';
+import data from './heros.json';
+
+
+function Card(props) {
+  return (
+     <div className="card">
+      <h2 className='hero_name'>Имя: {props.name}</h2>
+      <p className='hero_universe'>Вселенная: {props.universe}</p>
+      <p className='hero_alterego'>Альтерэго: {props.alterego}</p>
+      <p className='hero_occipation'>Род деятельности: {props.occipation}</p>
+      <p className='hero_friends'>Друзья: {props.friends}</p>
+      <p className='hero_superpower'>Суперспособности: {props.superpower}</p>
+      <p className='hero_info'>Инфо: {props.info}</p>
+      </div>
+       );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {data.map((i) => (
+                <Card
+                    name={i.name}
+                    universe={i.universe}
+                    alterego={i.alterego}
+                    occupation={i.occupation}
+                    friends={i.friends}
+                    superpowers={i.superpowers}
+                   // url={i.url}
+                    info={i.info}
+                />
+                             ))}
     </div>
   );
 }
